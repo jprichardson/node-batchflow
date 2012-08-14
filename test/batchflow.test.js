183d576ccfd9f88bbb524858c705c304d2dad003
JP Richardson <jprichardson@gmail.com>
2012-08-14 12:44:35 -0500

    Can now process arrays and objects in sequence or in parallel.

--- test/batchflow.test.js
+++ test/batchflow.test.js
@@ -107,47 +107,67 @@ describe('batchflow', function() {
             }
         };
 
-        /*describe('sequential', function() {
+        describe('sequential', function() {
             it('should execute sequentially', function(done) {
-                
+                var keys = [];
+
                 batch(o).sequential()
                 .each(function(key, val, done) {
+                    keys.push(key);
                     val(done);
                 }).end(function(results) {
-                    for (var i = 0; i < results.length; ++i) {
+                    /*for (var i = 0; i < results.length; ++i) {
                         console.log(results[i]);
-                    }
+                    }*/
                     
                     assert(results[0] === 1);
                     assert(results[1] === 2);
                     assert(results[2] === 3);
 
+                    assert(keys[0] === 'a');
+                    assert(keys[1] === 'b');
+                    assert(keys[2] === 'c');
+
+                    assert(results.length === 3);
+                    assert(keys.length === 3);
+
                     done();
                 });
             
             });
             
-        });*/
+        });
 
-        /*describe('parallel', function() {
+        describe('parallel', function() {
             it('should execute in parallel', function(done) {
-                batch(a).parallel()
-                .each(function(item, done) {
-                    item(done);
+                var keys = [];
+
+                batch(o).parallel()
+                .each(function(key, val, done) {
+                    keys.push(key);
+                    val(done);
                 }).end(function(results) {
-                    for (var i = 0; i < results.length; ++i) {
+                    //console.log
+                    /*for (var i = 0; i < results.length; ++i) {
                         console.log(results[i]);
-                    }
+                    }*/
 
                     assert(results[0] === 1);
                     assert(results[1] === 3);
                     assert(results[2] === 2);
 
+                    assert(keys[0] === 'a');
+                    assert(keys[1] === 'b');
+                    assert(keys[2] === 'c');
+
+                    assert(results.length === 3);
+                    assert(keys.length === 3);
+
                     done();
                 });
             });
 
-        });*/
+        });
     });
 
 
